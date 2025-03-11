8ce7bde07a1535978c5915899caa85185005feac
LeticiaDoffSottaSouza <leticiadoff@gmail.com>
2025-03-11 14:22:42 -0300

    Add files via upload

---
+++ hotel.cpp
@@ -0,0 +1,61 @@
+//biblioteca
+#include <stdio.h>
+#include <locale.h>
+//inicio
+int main (){
+    setlocale(LC_ALL,"");
+    char nome[20], sobrenome[20], cidade[20], pagamento[20];  
+    int idade, pessoas, numero, dias, opcao1, opcao2, opcao3;
+    float valor;
+    printf("Bem-vindo ao hotel Alika!\n");
+    printf("Informe seu nome: ");
+    scanf("%s", nome);
+    printf("Informe seu sobrenome: ");
+    scanf("%s", sobrenome);
+    printf("Informe sua idade: ");
+    scanf("%d", &idade);
+    printf("Informe a cidade em que você mora: ");
+    scanf("%s", cidade);
+    printf("Informe quantas pessoas ficarão hospedadas contando com você: ");
+    scanf("%d", &pessoas);
+    printf("Informe quantos dias pretende ficar hospedado: ");
+    scanf("%d", &dias);
+    printf("Informe seu número de telefone: ");
+    scanf("%d", &numero);
+    valor = dias * 350.00;
+    printf("O valor de sua hospedagem é de: %.2f \n", valor);
+    printf("Informe a forma de pagamento: ");
+    scanf("%s", pagamento);
+    printf("Selecione uma das opções:\n");
+    printf("1- Gostaria de solicitar o seviço de quarto!\n");
+    printf("2- No momento não quero solicitar o serviço de quarto.\n");
+    scanf("%d", &opcao1);
+    switch(opcao1){
+        case 1:
+        printf("1- Trocar roupa de cama\n");
+        printf("2- Limpar o banheiro\n");
+        printf("3- Limpar o quarto\n");
+        scanf("%d", &opcao2);
+        switch(opcao2){
+            case 1:
+            printf("Seu pedido será feito em breve!");
+            break;
+            case 2:
+            printf("Seu pedido será feito em breve!");
+            break;
+            case 3:
+            printf("Seu pedido será feito em breve!");
+            break;
+        }
+        break;
+        
+        case 2:
+        printf("Ok :)");
+    }
+    printf("Gostaria de fazer algum pedido?:\n");
+    printf("Sim\n");
+    printf("Não\n");
+    scanf("%d", &opcao3)
+    return 0;
+    
+}
